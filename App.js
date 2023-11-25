b2d88f611e2ad63a5b9598e81e097ce6832f6c2d
unknown <ai.ankit.one@gmail.com>
2023-11-25 14:56:23 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,30 @@
+/*
+<div id="parent">
+    <div id="child">
+        <h1></h1>
+        <h2></h2>
+    </div>
+    <div id="child2">
+        <h1></h1>
+        <h2></h2>
+    </div>
+</div>
+
+*/
+
+// const heading = React.createElement("h1", {id: "heading",xyz: "abc"},"Hello World from React !!")
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, [ 
+    React.createElement("h1", {}, "I am an h1 tag !!"),
+    React.createElement("h2", {}, "I am an h2 tag !!")
+    ]),
+    React.createElement("div", {id: "child2"}, [ 
+    React.createElement("h1", {}, "I am an h1 tag !!"),
+    React.createElement("h2", {}, "I am an h2 tag !!")
+    ])
+]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+console.log(parent);
+console.log(root);
+root.render(parent);
